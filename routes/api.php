4cd386ca121687b34982e48c74c68ba810cb65ff
Abreham Kassa <abrehamkassa19@gmail.com>
2025-03-05 11:28:37 +0300

    Define the endpoints to access books and post books

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/health-check', function () {
     return response()->json([
@@ -8,3 +9,10 @@ Route::get('/health-check', function () {
 	'message' => 'The API is working!',
     ]);
 });
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}',[BookController::class, 'destroy']);
+
